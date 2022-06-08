73c6b426054d097511fb91fb4cc66b28c08301c2
Avishkar Mohite <avimohite2019@gmail.com>
2022-06-08 09:33:57 +0530

    UI changes and directory set-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line
 
-function App() {
+import Home from "Home.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     < Home />
+  
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+// import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
+// import Features from "components/features/VerticalWithAlternateImageAndText.js";
+// import Blog from "components/blogs/ThreeColSimpleWithImage.js";
+// import Testimonial from "components/testimonials/TwoColumnWithImage.js";
+// import ContactUsForm from "components/forms/SimpleContactUs.js";
+// import Footer from "components/footers/SimpleFiveColumn.js";
+import Homepage from "screens/Homepage/Homepage";
+
+export default () => (
+  // <AnimationRevealPage>
+  //   <Hero />
+  //   <Features />
+  //   <Blog />
+  //   <Testimonial />
+  //   <ContactUsForm />
+  //   <Footer />
+  // </AnimationRevealPage>
+
+  <>
+  <Homepage />
+  </>
+);
